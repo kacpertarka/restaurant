58b42c848fa920fca7e177caff5ddadb37e9b877
Kacper Tarka <kacper.tarka00@gmail.com>
2024-11-13 19:05:52 +0100

    create user payload structure and add parse json function

--- services/users/handler.go
+++ services/users/handler.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/kacpertarka/restaurant/utils"
 )
 
 type UserHandler struct {
@@ -20,15 +21,29 @@ That handler is used by administration to creat new user account and manage all 
 */
 func (handler *UserHandler) RegisterRoutes(router *mux.Router) {
 	// handle methods like: add new user, get all users, get single user
-	router.HandleFunc("/users", handler.createNewUser).Methods("POST") 	// create new user
-	router.HandleFunc("/users", handler.getAllUsers).Methods("GET") 	// get all users
-	router.HandleFunc("/users/{user_id}", handler.getSingleUser).Methods("GET")	// get single user
+	router.HandleFunc("/users", handler.createNewUser).Methods("POST")          // create new user
+	router.HandleFunc("/users", handler.getAllUsers).Methods("GET")             // get all users
+	router.HandleFunc("/users/{user_id}", handler.getSingleUser).Methods("GET") // get single user
 }
 
 func (handler *UserHandler) createNewUser(w http.ResponseWriter, r *http.Request) {
-	// implement logic to create new user
-}
+	/*
+		Create new user account with given: email, first_name, last_name.
+		Set variable such as role (default is worker), created_at (created time),
+		Service should generate user_id (UUID), first login password (after first login it should be changed)
 
+		Return map with user_id and password if user created successfully.  Otherwise, return error message.
+	*/
+	// get JSON payload
+	var payload RegisterUserPayload
+	if err := utils.ParseJSON(r, payload); err != nil {
+		utils.WriteERROR(w, err)
+        return
+	}
+	// check if user with given email exists - worker with given email
+
+	// if doesn't exist - create new user and return map with user_id and password
+}
 
 func (handler *UserHandler) getAllUsers(w http.ResponseWriter, r *http.Request) {
 	// implement logic to get all users
--- utils/utils.go
+++ utils/utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -14,4 +15,13 @@ func WriteJSON(w http.ResponseWriter, statusCode int, data any) error {
 func WriteERROR(w http.ResponseWriter, err any) error {
 	error := map[string]any{"error": err}
 	return WriteJSON(w, http.StatusBadRequest, error)
+}
+
+func ParseJSON(r *http.Request, payload any) error {
+	// check if r.Body contains any data
+	defer r.Body.Close()
+	if r.Body == nil {
+		return fmt.Errorf("missing request data")
+	}
+	return json.NewDecoder(r.Body).Decode(payload)
 }
